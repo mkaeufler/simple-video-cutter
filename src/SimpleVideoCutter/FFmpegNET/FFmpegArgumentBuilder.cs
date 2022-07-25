cd9ab347335597bbc1d400ae7f1f75524e546c31
mkaeufler <mkaeufler@users.noreply.github.com>
2022-07-25 21:16:13 +0200

    fix for #59: Use output seeking instead of input seeking to fix incorrect start timestamps

--- src/SimpleVideoCutter/FFmpegNET/FFmpegArgumentBuilder.cs
+++ src/SimpleVideoCutter/FFmpegNET/FFmpegArgumentBuilder.cs
@@ -16,23 +16,25 @@ namespace SimpleVideoCutter.FFmpegNET
         {
             var commandBuilder = new StringBuilder();
 
-            // Warning! There is a difference when placin 'ss' before or after 'i'.
+            // Warning! There is a difference when placing 'ss' before or after 'i'.
             // See more in https://trac.ffmpeg.org/wiki/Seeking
+            // Using output seeking since input seeking might cause an issue where the start time of the resulting
+            // video equals the original timestamp of the starting cut instead of 0:00.
 
             if (lossless)
             {
                 // Maybe also use -noaccurate_seek ?
-                commandBuilder.AppendFormat(CultureInfo.InvariantCulture, " -ss {0:0.000} ", start.TotalSeconds);
                 commandBuilder.AppendFormat(" -i \"{0}\" ", inputFileFullPath);
                 commandBuilder.AppendFormat(" -codec copy -copyts ");
+                commandBuilder.AppendFormat(CultureInfo.InvariantCulture, " -ss {0:0.000} ", start.TotalSeconds);
                 commandBuilder.AppendFormat(CultureInfo.InvariantCulture, " -to {0:0.000} ", end.TotalSeconds);
                 commandBuilder.AppendFormat(" -map 0:v -map 0:a ");
             }
             else
             {
-                commandBuilder.AppendFormat(CultureInfo.InvariantCulture, " -ss {0:0.000} ", start.TotalSeconds);
                 commandBuilder.AppendFormat(" -i \"{0}\" ", inputFileFullPath);
                 commandBuilder.AppendFormat(" -copyts ");
+                commandBuilder.AppendFormat(CultureInfo.InvariantCulture, " -ss {0:0.000} ", start.TotalSeconds);
                 commandBuilder.AppendFormat(CultureInfo.InvariantCulture, " -to {0:0.000} ", end.TotalSeconds);
                 commandBuilder.AppendFormat(" -map 0:v -map 0:a ");
             }
